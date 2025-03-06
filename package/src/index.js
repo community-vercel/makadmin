a071d343ec2536201eec0d3b6b407626dc7a74fc
community-vercel <community-vercel>
2025-03-06 09:46:14 -0800

    aaa

--- package/src/index.js
+++ package/src/index.js
@@ -1,24 +1,20 @@
 import React, { Suspense } from "react";
-// import ReactDOM from "react-dom";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; // Change HashRouter to BrowserRouter
 import Loader from "./layouts/loader/Loader";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <Router>
+  <Router basename="/admin"> {/ Add basename /}
     <Suspense fallback={<Loader />}>
       <App />
     </Suspense>
   </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
