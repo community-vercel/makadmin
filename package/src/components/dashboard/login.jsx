ce9b2d407292bf9576e198f21b6e8339f3bbcd1d
community-vercel <community-vercel>
2025-03-06 13:30:52 -0800

    aaa

--- package/src/components/dashboard/login.jsx
+++ package/src/components/dashboard/login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
       
-        const response = await fetch('http://127.0.0.1:8000/api/login/', {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}login/`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ username, password }),
@@ -40,7 +40,7 @@ const Login = () => {
         textAlign: 'center',
     };
 
-    
+
 
     const formStyle = {
         display: 'flex',
